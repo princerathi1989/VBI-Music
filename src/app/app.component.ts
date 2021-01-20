a0d56be160ad5b0ae3c08606a2fe4a7f8aa19251
Prince Chaudhary <prince.chaudhary@blinkfitness.com>
2021-01-20 16:29:30 +0530

    Completed
    MUSIC APP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vbi-music';
+  data = { columns: ['thumbnail', 'title', 'album'], flow: 'all' };
+  constructor() { }
 }
+
